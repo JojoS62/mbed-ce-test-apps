b216648bc3cda7ff49dc831595b15ce1e606eb7f
Johannes Stratmann <sn2010@onlinehome.de>
2022-11-16 20:41:45 +0100

    add thread an event

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include "mbed.h"
 
-int main()
-{
-	while(true) 
-	{
-		printf("Hello world from Mbed CE!\n");
-		ThisThread::sleep_for(1s);
+Thread thread;
+Thread thread_events;
+EventQueue queue;
+InterruptIn user_button(BUTTON1);
+
+void thread_fn(){
+	DigitalOut led(LED1);
+
+	while(1){
+		led = !led;
+		ThisThread::sleep_for(50ms);
 	}
 
-	// main() is expected to loop forever.
-	// If main() actually returns the processor will halt
+}
+
+int main()
+{
+	thread.start(thread_fn);
+	thread_events.start(callback(&queue, &EventQueue::dispatch_forever));
+
+	user_button.rise( []() {
+		queue.call(printf, "ping\n");
+	});
+
+	int counter = 0;
+	while(true) 
+	{
+		printf("Hello world from Mbed CE! %d\n", counter);
+		ThisThread::sleep_for(1s);
+		counter++;
+	}
+
 	return 0;
 }
