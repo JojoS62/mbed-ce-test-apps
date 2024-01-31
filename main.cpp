6b94e3bb976f2edcaa310e04dcfae80eadde9e2d
Johannes Stratmann <sn2010@onlinehome.de>
2024-01-31 20:22:15 +0100

    add comments add cyclic call for queue

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ Thread thread;
 Thread thread_events;
 EventQueue queue;
 InterruptIn user_button(BUTTON1);
+DigitalOut led2(LED2);
 
 void thread_fn() {
 	DigitalOut led(LED1);
@@ -20,15 +21,24 @@ int main()
     printf("Hello from "  MBED_STRINGIFY(TARGET_NAME) "\n");
     printf("Mbed OS version: %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
-
+	// start a thread with blinking LED1
 	thread.start(thread_fn);
+
+	// start a thread with queue dispatcher
 	thread_events.start(callback(&queue, &EventQueue::dispatch_forever));
 
+	// add interrupt callback for button1, write messages to serial
 	user_button.rise( []() {
 		queue.call(printf, "ping\n");
 		queue.call_in(2s, printf, "pong\n");
 	});
 
+	// add a cyclic function call to queue
+	queue.call_every(200ms, []() {
+		led2 = !led2;
+	});
+
+	// main loop, print message with counter
 	int counter = 0;
 	while(true) 
 	{
