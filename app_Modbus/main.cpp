56ba2aefdc25cf4608064411be012b8b86c83b62
Johannes Stratmann <sn2010@onlinehome.de>
2024-02-14 19:35:33 +0100

    Removing all files in .gitignore

--- app_Modbus/main.cpp
+++ app_Modbus/main.cpp
@@ -9,37 +9,15 @@ DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 
 
-void thread_fn() {
-	DigitalOut led(LED1);
-
-	while(1) {
-		led = !led;
-		ThisThread::sleep_for(500ms);
-	}
-}
-
 int main()
 {
     printf("mbed-ce modbus-slave\n");
     printf("Hello from "  MBED_STRINGIFY(TARGET_NAME) "\n");
     printf("Mbed OS version: %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
-	// start a thread with blinking LED1
-	thread.start(thread_fn);
 
 	// start a thread with queue dispatcher
 	thread_events.start(callback(&queue, &EventQueue::dispatch_forever));
-
-	// add interrupt callback for button1, write messages to serial
-	user_button.rise( []() {
-		queue.call(printf, "ping\n");
-		queue.call_in(2s, printf, "pong\n");
-	});
-
-	// add a cyclic function call to queue
-	queue.call_every(200ms, []() {
-		led2 = !led2;
-	});
 
 	// Enable the Modbus Protocol Stack.
     eMBErrorCode    eStatus;
