a3ea978cbf9db00ec47a46e620f2cc85f766afed
Johannes Stratmann <sn2010@onlinehome.de>
2024-03-02 21:17:51 +0100

    add styles.css

--- app_Fingerprint/www/update.sh
+++ app_Fingerprint/www/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 atftp  -r /sda/index.html -l index.html --put 192.168.100.134 
 atftp  -r /sda/fingerprint.js -l fingerprint.js --put 192.168.100.134 
+atftp  -r /sda/styles.css -l styles.css --put 192.168.100.134 
